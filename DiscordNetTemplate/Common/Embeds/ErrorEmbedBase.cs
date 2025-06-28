4cee48bea98fd13023ea30ae33c331c2249768af
Solar M. <me@benjmainmacaulay.com>
2025-06-28 17:43:40 +0100

    fix: fixed to use url for embed bases

--- DiscordNetTemplate/Common/Embeds/ErrorEmbedBase.cs
+++ DiscordNetTemplate/Common/Embeds/ErrorEmbedBase.cs
@@ -1,11 +1,12 @@
 using Discord;
 using DiscordNetTemplate.Assets;
+using Emote = DiscordNetTemplate.Assets.Emote;
 
 namespace DiscordNetTemplate.Common.Embeds;
 
 public class ErrorEmbedBase : EmbedBase
 {
     public override string Name { get; } = "Error";
-    public override string? IconUrl { get; } = Emojis.Reactions.Cross;
+    public override string? IconUrl { get; } = Emote.Reactions.Cross.Url;
     public override Color Color { get; } = Colours.Error;
 }
--- DiscordNetTemplate/Common/Embeds/SuccessEmbedBase.cs
+++ DiscordNetTemplate/Common/Embeds/SuccessEmbedBase.cs
@@ -1,11 +1,12 @@
 using Discord;
 using DiscordNetTemplate.Assets;
+using Emote = DiscordNetTemplate.Assets.Emote;
 
 namespace DiscordNetTemplate.Common.Embeds;
 
 public class SuccessEmbedBase : EmbedBase
 {
     public override string Name { get; } = "Succeed!";
-    public override string? IconUrl { get; } = Emojis.Reactions.Check;
+    public override string? IconUrl { get; } = Emote.Reactions.Check.Url;
     public override Color Color { get; } = Colours.Success;
 }
