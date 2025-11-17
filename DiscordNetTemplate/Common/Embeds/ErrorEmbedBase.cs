6a5243d8557120558a712dae001a652782d5f6b3
Solar M. <me@benjmainmacaulay.com>
2025-11-17 00:50:52 +0000

    feat: add singleton instances to ErrorEmbedBase and SuccessEmbedBase

--- DiscordNetTemplate/Common/Embeds/ErrorEmbedBase.cs
+++ DiscordNetTemplate/Common/Embeds/ErrorEmbedBase.cs
@@ -6,6 +6,9 @@ namespace DiscordNetTemplate.Common.Embeds;
 
 public class ErrorEmbedBase : EmbedBase
 {
+    
+    public static ErrorEmbedBase Instance { get; } = new();
+    
     public override string Name { get; } = "Error";
     public override string? IconUrl { get; } = Emote.Reactions.Cross.Url;
     public override Color Color { get; } = Colours.Error;
--- DiscordNetTemplate/Common/Embeds/SuccessEmbedBase.cs
+++ DiscordNetTemplate/Common/Embeds/SuccessEmbedBase.cs
@@ -6,6 +6,9 @@ namespace DiscordNetTemplate.Common.Embeds;
 
 public class SuccessEmbedBase : EmbedBase
 {
+    
+    public static SuccessEmbedBase Instance { get; } = new();
+    
     public override string Name { get; } = "Succeed!";
     public override string? IconUrl { get; } = Emote.Reactions.Check.Url;
     public override Color Color { get; } = Colours.Success;
